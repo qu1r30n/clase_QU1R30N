8fcfec3fe7d2d28311928f67c57d40691816e9f1
qu1r30n <qu1r30n@hotmail.com>
2024-06-11 01:37:38 -0600

    todavia le falta mucho pero meti lo de sucursales para que si solo queremos ser el medio de conexion

---
+++ clase_QU1R30N/sin_internet/sin_formularios/modelos/_7_modelo_sucursales.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using clase_QU1R30N.sin_internet.sin_formularios.herramientas;
+using clase_QU1R30N.sin_internet.sin_formularios.procesos;
+
+namespace clase_QU1R30N.sin_internet.sin_formularios.modelos
+{
+    internal class _7_modelo_sucursales
+    {
+        string[] G_caracter_separacion = var_fun_GG.GG_caracter_separacion;
+        string[] G_caracter_separacion_funciones_espesificas = var_fun_GG.GG_caracter_separacion_funciones_espesificas;
+        string[] G_caracter_para_confirmacion_o_error = var_fun_GG.GG_caracter_para_confirmacion_o_error;
+
+        string[] G_direcciones =
+        {
+            Tex_base.GG_dir_bd_y_valor_inicial_bidimencional[10, 0]//"config\\inf\\inventario\\inventario.txt",
+        };
+
+        _7_procesos_sucursales pr_sucursales = new _7_procesos_sucursales();
+        public string operacion_a_hacer(string operacion)
+        {
+            string info_a_retornar = null;
+            string[] a_donde_enviara_la_informacion = operacion.Split(G_caracter_separacion_funciones_espesificas[1][0]);
+
+            switch (a_donde_enviara_la_informacion[0])
+            {
+                case "agregar":
+                    pr_sucursales.funcion_a_hacer1(G_direcciones[0], "", "", "");
+                    break;
+
+                default:
+                    info_a_retornar = "3" + G_caracter_para_confirmacion_o_error[0] + "no existe ese PROCESO";
+                    break;
+            }
+
+            return info_a_retornar;
+
+        }
+
+    }
+}
---
+++ clase_QU1R30N/sin_internet/sin_formularios/procesos/_7_procesos_sucursales.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using clase_QU1R30N.sin_internet.sin_formularios.herramientas;
+
+namespace clase_QU1R30N.sin_internet.sin_formularios.procesos
+{
+    internal class _7_procesos_sucursales
+    {
+        string[] G_caracter_separacion = var_fun_GG.GG_caracter_separacion;
+        string[] G_caracter_separacion_funciones_espesificas = var_fun_GG.GG_caracter_separacion_funciones_espesificas;
+        string[] G_caracter_para_confirmacion_o_error = var_fun_GG.GG_caracter_para_confirmacion_o_error;
+
+        var_fun_GG vf_GG = new var_fun_GG();
+
+        int G_donde_inicia_la_tabla = var_fun_GG.GG_indice_donde_comensar;
+
+        Tex_base bas = new Tex_base();
+
+        public string funcion_a_hacer1(string direccion_archivo, string parametro1, string parametro2, string parametro3, object caracter_separacion_obj = null)
+        {
+
+            string[] caracter_separacion_string = vf_GG.GG_funcion_caracter_separacion(caracter_separacion_obj);
+            string info_a_retornar = "";
+
+
+            string direccion_archivo_aprendices_emp = direccion_archivo;
+            string resultado_archivo_aprendices_emp = bas.sacar_indice_del_arreglo_de_direccion(direccion_archivo_aprendices_emp);
+            string[] res_esp_archivo_emp = resultado_archivo_aprendices_emp.Split(G_caracter_para_confirmacion_o_error[0][0]);
+            if (res_esp_archivo_emp[0] != "0")
+            {
+
+
+            }
+
+            else
+            {
+                info_a_retornar = "0" + G_caracter_para_confirmacion_o_error[0] + "no se encontro la direccion";
+            }
+
+            return info_a_retornar;
+        }
+
+
+
+    }
+}
