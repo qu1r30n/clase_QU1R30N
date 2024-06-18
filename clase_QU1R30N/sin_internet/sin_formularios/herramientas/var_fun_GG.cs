04dc80077a099fa7b9709f43398feff09f74f6d2
qu1r30n <qu1r30n@hotmail.com>
2024-06-17 21:23:06 -0600

    empieso a pasar lo de los archivos iniciales a aserlo estatico

--- clase_QU1R30N/sin_internet/sin_formularios/herramientas/var_fun_GG.cs
+++ clase_QU1R30N/sin_internet/sin_formularios/herramientas/var_fun_GG.cs
@@ -55,8 +55,6 @@ namespace clase_QU1R30N.sin_internet.sin_formularios.herramientas
 
         }
 
-        
-
         public string[] GG_funcion_caracter_separacion_funciones_especificas(object caracter_separacion_objeto = null)
         {
             string[] caracter_separacion = null;
@@ -103,6 +101,179 @@ namespace clase_QU1R30N.sin_internet.sin_formularios.herramientas
             return caracter_separacion;
 
         }
+
+
+
+        static public string[] GG_variables_string =
+        {
+           /*0*/ "",//tex_esplit[0]//codbar
+           /*1*/ "",//prov_anterior
+           /*2*/ "", //provedores_txt//todos_los_provedores
+           /*3*/ "",//impuesto anterior
+           /*4*/ "", //impuestos_txt//todos_los_impuestos
+           /*5*/ "",//tipo_medida_producto_anterior
+           /*6*/ ""//tipo_medida_producto_txt//todos_los_tipos_de_medida
+           
+        };
+
+        //funciones y restricciones txt y cmb ventana_emergente cod:poison
+        ////////////////////////////////////////////////////////////////////////
+        //                                SI EDITAS                           //
+        //                      [,] GG_ventana_emergente_productos            //
+        //                             TIENES QUE EDITAR                      //
+        //                      RecargarVentanaEmergenteProductos             //
+        //                          ES EL DE ABAJITO A ESTE                   //
+        ////////////////////////////////////////////////////////////////////////
+        static public string[,] GG_ventana_emergente_productos = new string[,]
+        {
+            /*0*/ { "2", "_0_id", "" },
+            /*1*/ { "1", "_1_producto", "" },
+            /*2*/ { "1", "_2_contenido", "0|solo_numeros" },
+            /*3*/ { "4", "_3_tipo_medida", "NOSE|todas_mayusculas|" + GG_variables_string[5] + '|' + GG_variables_string[6] },
+            /*4*/ { "1", "_4_precio_venta", "0|solo_numeros" },
+            /*5*/ { "2", "_5_cod_barras", GG_variables_string[0] },
+            /*6*/ { "1", "_6_cantidad", "1|solo_numeros" },
+            /*7*/ { "1", "_7_costo_comp", "0|solo_numeros" },
+            /*8*/ { "4", "_8_provedor", "NOSE|todas_mayusculas|" + GG_variables_string[1] + '|' + GG_variables_string[2] },
+            /*9*/ { "4", "_9_grupo", "1||1|1°2°producto_elaborado°venta_ingrediente|ocultar_control¬23¬producto_elaborado°ocultar_control¬29¬venta_ingrediente" },
+            /*10*/ { "2", "_10_no_poner_nada", "" },
+            /*11*/ { "1", "_11_cant_produc_x_paquet", "1|solo_numeros" },
+            /*12*/ { "1", "_12_tipo_de_producto", "||||no_visible°producto_elaborado" },
+            /*13*/ { "1", "_13_ligar_produc_sab", "" },
+            /*14*/ { "1", "_14_impuestos", "|todas_mayusculas|||reyeno_textbox_ventana_impu" },
+            /*15*/ { "1", "_15_si_es_elaborado_que_materia_prima_usa_y_cantidad", "||||no_visible°venta_ingrediente" },
+            /*16*/ { "1", "_16_caducidad", "0|solo_numeros" },
+            /*17*/ { "1", "_17_ultimo_movimiento", "0|solo_numeros" },
+            /*18*/ { "1", "_18_sucursal_vent", "" },
+            /*19*/ { "1", "_19_clasificacion_producto", "" },
+        };
+
+        public static void RecargarVentanaEmergenteProductos(string al_finalizar_que_borrar_para_proxima_ventana = "")
+        {
+            GG_ventana_emergente_productos = new string[,]
+            {
+                /*0*/ { "1", "producto", "" },
+                /*1*/ { "1", "contenido", "0|solo_numeros" },
+                /*2*/ { "4", "tipo_medida", "NOSE|todas_mayusculas|" + GG_variables_string[5] + '|' + GG_variables_string[6] },
+                /*3*/ { "1", "precio_venta", "0|solo_numeros" },
+                /*4*/ { "2", "cod_barras", GG_variables_string[0] },
+                /*5*/ { "1", "cantidad", "1|solo_numeros" },
+                /*6*/ { "1", "costo_comp", "0|solo_numeros" },
+                /*7*/ { "4", "provedor", "NOSE|todas_mayusculas|" + GG_variables_string[1] + '|' + GG_variables_string[2] },
+                /*8*/ { "4", "grupo", "1||1|1°2°producto_elaborado°venta_ingrediente|ocultar_control¬23¬producto_elaborado°ocultar_control¬29¬venta_ingrediente" },
+                /*9*/ { "2", "no poner nada", "" },
+                /*10*/ { "1", "cant_produc_x_paquet", "1|solo_numeros" },
+                /*11*/ { "1", "tipo_de_producto", "||||no_visible°producto_elaborado" },
+                /*12*/ { "1", "ligar_produc_sab", "" },
+                /*13*/ { "1", "impuestos", "|todas_mayusculas|||reyeno_textbox_ventana_impu" },
+                /*14*/ { "1", "parte_de_que_producto", "||||no_visible°venta_ingrediente" }
+            };
+
+
+
+
+
+            if (al_finalizar_que_borrar_para_proxima_ventana != null)
+            {
+
+
+                string[] datos_a_borrar = al_finalizar_que_borrar_para_proxima_ventana.ToString().Split(Convert.ToChar(GG_caracter_separacion[0]));
+
+                for (int i = 0; i < datos_a_borrar.Length; i++)
+                {
+
+                    if (datos_a_borrar[i] == "todo")
+                    {
+                        GG_variables_string = new[]
+                        {
+                            /*0*/ "",//tex_esplit[0]//codbar
+                            /*1*/ "",//prov_anterior
+                            /*2*/ "", //provedores_txt//todos_los_provedores
+                            /*3*/ "",//impuesto anterior
+                            /*4*/ "", //impuestos_txt//todos_los_impuestos
+                            /*5*/ "",//tipo_medida_producto_anterior
+                            /*6*/ ""//tipo_medida_producto_txt//todos_los_tipos_de_medida
+           
+                        };
+                    }
+
+                    else if (datos_a_borrar[i] == "") { }
+
+                    else
+                    {
+                        GG_variables_string[Convert.ToInt32(datos_a_borrar[i])] = "";
+                    }
+
+                }
+            }
+
+        }
+
+        //lo mismo ventana provedor
+        static public string[,] GG_ventana_provedor =
+        {
+            { "1","0_ID_EMPRESA","" },
+            { "1","1_NOMBRE_EMPRESA","" },
+            { "1","2_NOMBRE_ENCARGADO","" },
+            { "1","3_DIRECCIÓN_EMPRESA","" },
+            { "1","4_CIUDAD_EMPRESA","" },
+            { "1","5_ESTADO_EMPRESA","" },
+            { "1","6_CÓDIGO_POSTAL","" },
+            { "1","7_PAÍS","" },
+            { "1","8_CORREO_ELECTRÓNICO","" },
+            { "1","9_TELÉFONO_ENCARGADO","" },
+            { "1","10_TELEFONO_EMPRESA","" },
+            { "1","11_TIPO_DE_PROVEEDOR","" },
+            { "1","12_PRODUCTOS_SERVICIOS_SUMINISTRADOS","" },
+            { "1","13_CUENTA_BANCO","" },
+            { "1","14_UBICACIÓN_(GPS)","" },
+            { "1","15_NOTAS","" },
+            { "1","16_RECORDATORIO","" },
+            { "1","17_ACTIVO_O_NO_ACTIVO","" },
+            { "1","calificacion_preventa¬0°calificacion_entrega¬0","" },
+            { "1","comentarios_preventa_entrega","" },
+            { "1","dinero","0|solo_numeros" },
+            { "1","dias_de_preventa_0°dias_de_preventa_1","" },
+            { "1","dias_de_entrega_0°dias_de_entrega_1","" },
+            
+        };
+
+
+        public static string columnas_concatenadas(string[,] arreglo_bidimencional, int id_columna, string caracter_separacion = null)
+        {
+            if (caracter_separacion == null)
+            {
+                caracter_separacion = GG_caracter_separacion[0];
+            }
+            string nombresConcatenados = "";
+
+            for (int i = 0; i < arreglo_bidimencional.GetLength(0); i++)
+            {
+                string nombre = arreglo_bidimencional[i, id_columna];
+                nombresConcatenados += nombre + Convert.ToChar(GG_caracter_separacion[0]);
+            }
+
+            if (!string.IsNullOrEmpty(nombresConcatenados))
+            {
+                nombresConcatenados = nombresConcatenados.TrimEnd(Convert.ToChar(GG_caracter_separacion[0]));
+            }
+
+            return nombresConcatenados;
+        }
+
+
+        static public string[] GG_direccion_base = { "" };
+        static public string[,] GG_dir_nom_archivos =
+        {
+            /*0*/{ GG_direccion_base[0]+"CONFIG\\INF\\INVENTARIO\\INVENTARIO.TXT", columnas_concatenadas(GG_ventana_emergente_productos,1,GG_caracter_separacion[0])},
+            /*0*/{ GG_direccion_base[0]+"CONFIG\\INF\\DAT\\PROVEDORES.TXT", columnas_concatenadas(GG_ventana_provedor,1,GG_caracter_separacion[0])},
+
+        };
+
+
+
+
+
 
         //movimientos a repetir esto es mas para la aplicacion del celular para pasarlo al de la computadora y mejorar
         //tienes que poner en datos la informacion de_las_variables si es un arreglo usa el join paresido y agregale un GG_separador_para_funciones_espesificas
